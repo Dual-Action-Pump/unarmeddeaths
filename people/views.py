e075fb3fd154441af0c2c0bc2c40979082aea75e
Mauricio Morales <mmorale8@binghamton.edu>
2017-01-24 18:28:20 -0500

    First commit. Set up loop that displays dates of unarmed shootings and gives back a count.

---
+++ people/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import HttpResponse, render
+
+import json
+import requests
+
+def index(request):
+
+    persons = requests.get('https://thecountedapi.com/api/counted')
+    json_persons = persons.text
+    persons_info = json.loads(json_persons)
+    parsedData = []
+    for data in persons_info[:5000]:
+        temp_dict = {}
+        if data["armed"] == "No":
+            temp_dict["day"] = data["day"]
+            temp_dict["month"] = data["month"]
+            temp_dict["year"] = data["year"]
+            parsedData.append(temp_dict)
+    print(len(parsedData))
+
+    return render(request, "people/index.html", {"peoples": parsedData})
